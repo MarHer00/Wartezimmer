3bfb0b110137f90c2e342f118b138a5ab2f0bc3c
MarHer00 <89139260+MarHer00@users.noreply.github.com>
2021-08-31 11:11:56 +0200

    Update Wartezimmer.java

--- Wartezimmer.java
+++ Wartezimmer.java
@@ -1,11 +1,25 @@
 public class Wartezimmer<Patient>{// in <> ggf. was anderes schreiben, da ich mir über die Richtigkeit von Patient hier nicht sicher bin
 	
-	Warteplatz first;
-	Warteplatz last;
-	Warteplatz current;
+	Warteplatz erster;
+	Warteplatz letzter;
+	Warteplatz aktueller;
+	int wartezimmer_length = 0;
 	
 	private class Warteplatz{
 		 
 	}
+	
+	public void einfuegen_anErsterStelle(Patient neuerPatient){
+		wartezimmer_length++;
+	}
+	
+	public void einfuegen_anLetzterStelle(Patient neuerPatient){
+		wartezimmer_length++;
+	}
+	
+	public void entfernen(){
+		wartezimmer_length--;
+	}
+	
 	
 }
